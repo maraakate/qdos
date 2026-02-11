509681f90e2da612f0152ad67c939b14e9af2478
maraakate <emoaddict15@gmail.com>
2026-02-10 19:23:36 -0500

    Add the PS/2 mouse code from Q2DOS, etc.

--- common/in_dos.c
+++ common/in_dos.c
@@ -17,6 +17,18 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+/*
+PS/2 sample rate and resolution modification code Copyright 2026 by Frank Sapone.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 // in_mouse.c -- dos mouse code
 
 #include <dos.h>
@@ -47,6 +59,8 @@ cvar_t	*m_filter;
 cvar_t	*in_joystick;
 cvar_t	*joy_numbuttons;
 cvar_t	*aux_look;
+cvar_t	*m_ps2_sample_rate;
+cvar_t	*m_ps2_resolution;
 
 static qboolean	mouse_avail;
 static qboolean mouseactive;
@@ -57,6 +71,7 @@ static	int		mouse_buttonstate;
 static	int		mouse_wheelcounter;
 static	float	mouse_x, mouse_y;
 static	float	old_mouse_x, old_mouse_y;
+static qboolean nops2 = false;
 
 qboolean	joy_avail;
 int		joy_oldbuttonstate;
@@ -79,6 +94,9 @@ void IN_ExternalMove (usercmd_t *cmd);
 void IN_StartupJoystick (void);
 qboolean IN_ReadJoystick (void);
 
+static void IN_MouseSetPS2Rate (void);
+static void IN_MouseSetPS2Resolution (void);
+static const char *IN_MouseGetPS2ResolutionString (short val);
 
 void Toggle_AuxLook_f (void)
 {
@@ -114,8 +132,10 @@ static void IN_StartupMouse (void)
 		mouse_buttons = 3;
 	Con_Printf("%d-button mouse available\n", mouse_buttons);
 	mouseactive = true;
+
 	if (COM_CheckParm ("-nowheel"))
 		return;
+
 	regs.x.ax = 0x11;
 	dos_int86(0x33);
 	if (regs.x.ax == 0x574D && regs.h.cl == 1)
@@ -123,6 +143,32 @@ static void IN_StartupMouse (void)
 		mouse_wheel = true;
 		Con_Printf("mouse wheel support available\n");
 	}
+
+	if (COM_CheckParm ("-nops2"))
+	{
+		nops2 = true;
+		return;
+	}
+
+	regs.x.ax = 0xC206;
+	regs.h.dl = 0;
+	regs.h.cl = 0;
+	dos_int86(0x15);
+	if (regs.h.dl)
+	{
+		Con_Printf("PS/2 mouse resolution: %s.  sample rate: %d.\n", IN_MouseGetPS2ResolutionString(regs.h.cl), regs.h.dl);
+	}
+	else
+	{
+		/* FS: If we got nothing back then there was no PS/2 mouse plugged in at POST.  Sucks. */
+		nops2 = true;
+	}
+
+	IN_MouseSetPS2Rate();
+	m_ps2_sample_rate->modified = false;
+
+	IN_MouseSetPS2Resolution();
+	m_ps2_resolution->modified = false;
 }
 
 void IN_Init (void)
@@ -134,6 +180,8 @@ void IN_Init (void)
 	in_joystick = Cvar_Get("joystick","1", 0);
 	joy_numbuttons = Cvar_Get("joybuttons","4", CVAR_ARCHIVE);
 	aux_look = Cvar_Get("auxlook","1", CVAR_ARCHIVE);
+	m_ps2_sample_rate = Cvar_Get("m_ps2_sample_rate", "-1", 0);
+	m_ps2_resolution = Cvar_Get("m_ps2_resolution", "-1", 0);
 
 	Cmd_AddCommand ("toggle_auxlook", Toggle_AuxLook_f);
 	Cmd_AddCommand ("force_centerview", Force_CenterView_f);
@@ -152,6 +200,12 @@ void IN_Init (void)
 
 void IN_Shutdown (void)
 {
+	if (!nops2)
+	{
+		/* FS: If we fooled with the PS/2 sample rate and resolution settings then fix it. */
+		regs.x.ax = 0xC201;
+		dos_int86(0x15);
+	}
 }
 
 void IN_Commands (void)
@@ -252,6 +306,16 @@ void IN_MouseMove (usercmd_t *cmd)
 
 	if (!mouse_avail || !mouseactive)
 		return;
+
+	if (m_ps2_sample_rate->modified)
+	{
+		IN_MouseSetPS2Rate();
+	}
+
+	if (m_ps2_resolution->modified)
+	{
+		IN_MouseSetPS2Resolution();
+	}
 
 	IN_ReadMouseMove (&mx, &my);
 
@@ -608,3 +672,110 @@ void IN_ExternalMove (usercmd_t *cmd)
 		V_StopPitchDrift ();
 }
 
+static void IN_MouseSetPS2Rate (void)
+{
+	short rate;
+
+	if (nops2 || m_ps2_sample_rate->intValue == -1)
+	{
+		return; /* FS: Don't do anything. */
+	}
+
+	if (m_ps2_sample_rate->intValue < 0)
+		rate = 0x00;
+	else if (m_ps2_sample_rate->intValue > 6)
+		rate = 0x06;
+	else
+		rate = (short)m_ps2_sample_rate->intValue;
+
+	Con_Printf("Setting PS/2 sample rate to ");
+	switch (rate)
+	{
+		case 0:
+			Con_Printf("10hz\n");
+			break;
+		case 1:
+			Con_Printf("20hz\n");
+			break;
+		case 2:
+			Con_Printf("40hz\n");
+			break;
+		case 3:
+			Con_Printf("60hz\n");
+			break;
+		case 4:
+			Con_Printf("80hz\n");
+			break;
+		case 5:
+			Con_Printf("100hz\n");
+			break;
+		case 6:
+			Con_Printf("200hz\n");
+			break;
+		default:
+			Con_Printf("UNKNOWN!  THIS IS AN ERROR!\n");
+			break;
+	}
+
+	regs.h.ah = 0;
+	regs.x.ax = 0xC202;
+	regs.h.bh = rate;
+
+	dos_int86(0x15);
+	if (regs.h.ah)
+	{
+		Con_Printf("Set PS/2 sample rate failed: %d\n", regs.h.ah);
+	}
+
+	m_ps2_sample_rate->modified = false;
+}
+
+static void IN_MouseSetPS2Resolution (void)
+{
+	short resolution;
+
+	if (nops2 || m_ps2_resolution->intValue == -1)
+	{
+		return; /* FS: Don't do anything. */
+	}
+
+	if (m_ps2_resolution->intValue < 0)
+		resolution = 0x00;
+	else if (m_ps2_resolution->intValue > 3)
+		resolution = 0x03;
+	else
+		resolution = (short)m_ps2_resolution->intValue;
+
+	Con_Printf("Setting PS/2 resolution to %s\n", IN_MouseGetPS2ResolutionString(resolution));
+
+	regs.h.ah = 0;
+	regs.x.ax = 0xC203;
+	regs.h.bh = resolution;
+
+	dos_int86(0x15);
+	if (regs.h.ah)
+	{
+		Con_Printf("Set PS/2 resolution failed: %d\n", regs.h.ah);
+	}
+
+	m_ps2_resolution->modified = false;
+}
+
+static const char *IN_MouseGetPS2ResolutionString (short val)
+{
+	switch (val)
+	{
+		case 0:
+			return "one count per mm";
+		case 1:
+			return "two counts per mm";
+		case 2:
+			return "four counts per mm";
+		case 3:
+			return "eight counts per mm";
+		default:
+			return "UNKNOWN!  THIS IS AN ERROR!";
+	}
+
+	return "UNKNOWN!  THIS IS AN ERROR!\n";
+}
