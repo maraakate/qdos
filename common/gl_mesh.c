2df08a1e48d7dd8e1e712f3defa845eb9263c076
maraakate <emoaddict15@gmail.com>
2026-09-13 18:51:52 -0400

    Fix dumb spacing.

--- common/gl_mesh.c
+++ common/gl_mesh.c
@@ -8,7 +8,7 @@ of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 See the GNU General Public License for more details.
 
@@ -29,8 +29,8 @@ ALIAS MODEL DISPLAY LIST GENERATION
 =================================================================
 */
 
-model_t		*aliasmodel;
-aliashdr_t	*paliashdr;
+model_t *aliasmodel;
+aliashdr_t *paliashdr;
 
 qboolean	used[8192];
 
@@ -59,34 +59,34 @@ int	StripLength (int starttri, int startv)
 {
 	int			m1, m2;
 	int			j;
-	mtriangle_t	*last, *check;
+	mtriangle_t *last, *check;
 	int			k;
 
 	used[starttri] = 2;
 
 	last = &triangles[starttri];
 
-	stripverts[0] = last->vertindex[(startv)%3];
-	stripverts[1] = last->vertindex[(startv+1)%3];
-	stripverts[2] = last->vertindex[(startv+2)%3];
+	stripverts[0] = last->vertindex[(startv) % 3];
+	stripverts[1] = last->vertindex[(startv + 1) % 3];
+	stripverts[2] = last->vertindex[(startv + 2) % 3];
 
 	striptris[0] = starttri;
 	stripcount = 1;
 
-	m1 = last->vertindex[(startv+2)%3];
-	m2 = last->vertindex[(startv+1)%3];
+	m1 = last->vertindex[(startv + 2) % 3];
+	m2 = last->vertindex[(startv + 1) % 3];
 
 	// look for a matching triangle
 nexttri:
-	for (j=starttri+1, check=&triangles[starttri+1] ; j<pheader->numtris ; j++, check++)
+	for (j = starttri + 1, check = &triangles[starttri + 1]; j < pheader->numtris; j++, check++)
 	{
 		if (check->facesfront != last->facesfront)
 			continue;
-		for (k=0 ; k<3 ; k++)
+		for (k = 0; k < 3; k++)
 		{
 			if (check->vertindex[k] != m1)
 				continue;
-			if (check->vertindex[ (k+1)%3 ] != m2)
+			if (check->vertindex[(k + 1) % 3] != m2)
 				continue;
 
 			// this is the next part of the fan
@@ -97,11 +97,11 @@ nexttri:
 
 			// the new edge
 			if (stripcount & 1)
-				m2 = check->vertindex[ (k+2)%3 ];
+				m2 = check->vertindex[(k + 2) % 3];
 			else
-				m1 = check->vertindex[ (k+2)%3 ];
+				m1 = check->vertindex[(k + 2) % 3];
 
-			stripverts[stripcount+2] = check->vertindex[ (k+2)%3 ];
+			stripverts[stripcount + 2] = check->vertindex[(k + 2) % 3];
 			striptris[stripcount] = j;
 			stripcount++;
 
@@ -112,7 +112,7 @@ nexttri:
 done:
 
 	// clear the temp used flags
-	for (j=starttri+1 ; j<pheader->numtris ; j++)
+	for (j = starttri + 1; j < pheader->numtris; j++)
 		if (used[j] == 2)
 			used[j] = 0;
 
@@ -128,35 +128,35 @@ int	FanLength (int starttri, int startv)
 {
 	int		m1, m2;
 	int		j;
-	mtriangle_t	*last, *check;
+	mtriangle_t *last, *check;
 	int		k;
 
 	used[starttri] = 2;
 
 	last = &triangles[starttri];
 
-	stripverts[0] = last->vertindex[(startv)%3];
-	stripverts[1] = last->vertindex[(startv+1)%3];
-	stripverts[2] = last->vertindex[(startv+2)%3];
+	stripverts[0] = last->vertindex[(startv) % 3];
+	stripverts[1] = last->vertindex[(startv + 1) % 3];
+	stripverts[2] = last->vertindex[(startv + 2) % 3];
 
 	striptris[0] = starttri;
 	stripcount = 1;
 
-	m1 = last->vertindex[(startv+0)%3];
-	m2 = last->vertindex[(startv+2)%3];
+	m1 = last->vertindex[(startv + 0) % 3];
+	m2 = last->vertindex[(startv + 2) % 3];
 
 
 	// look for a matching triangle
 nexttri:
-	for (j=starttri+1, check=&triangles[starttri+1] ; j<pheader->numtris ; j++, check++)
+	for (j = starttri + 1, check = &triangles[starttri + 1]; j < pheader->numtris; j++, check++)
 	{
 		if (check->facesfront != last->facesfront)
 			continue;
-		for (k=0 ; k<3 ; k++)
+		for (k = 0; k < 3; k++)
 		{
 			if (check->vertindex[k] != m1)
 				continue;
-			if (check->vertindex[ (k+1)%3 ] != m2)
+			if (check->vertindex[(k + 1) % 3] != m2)
 				continue;
 
 			// this is the next part of the fan
@@ -166,9 +166,9 @@ nexttri:
 				goto done;
 
 			// the new edge
-			m2 = check->vertindex[ (k+2)%3 ];
+			m2 = check->vertindex[(k + 2) % 3];
 
-			stripverts[stripcount+2] = m2;
+			stripverts[stripcount + 2] = m2;
 			striptris[stripcount] = j;
 			stripcount++;
 
@@ -179,7 +179,7 @@ nexttri:
 done:
 
 	// clear the temp used flags
-	for (j=starttri+1 ; j<pheader->numtris ; j++)
+	for (j = starttri + 1; j < pheader->numtris; j++)
 		if (used[j] == 2)
 			used[j] = 0;
 
@@ -211,7 +211,7 @@ void BuildTris (void)
 	numorder = 0;
 	numcommands = 0;
 	memset (used, 0, sizeof(used));
-	for (i=0 ; i<pheader->numtris ; i++)
+	for (i = 0; i < pheader->numtris; i++)
 	{
 		// pick an unused triangle and start the trifan
 		if (used[i])
@@ -219,10 +219,10 @@ void BuildTris (void)
 
 		bestlen = 0;
 		besttype = 0;
-		for (type = 0 ; type < 2 ; type++)
-//	type = 1;
+		for (type = 0; type < 2; type++)
+			//	type = 1;
 		{
-			for (startv =0 ; startv < 3 ; startv++)
+			for (startv = 0; startv < 3; startv++)
 			{
 				if (type == 1)
 					len = StripLength (i, startv);
@@ -232,24 +232,24 @@ void BuildTris (void)
 				{
 					besttype = type;
 					bestlen = len;
-					for (j=0 ; j<bestlen+2 ; j++)
+					for (j = 0; j < bestlen + 2; j++)
 						bestverts[j] = stripverts[j];
-					for (j=0 ; j<bestlen ; j++)
+					for (j = 0; j < bestlen; j++)
 						besttris[j] = striptris[j];
 				}
 			}
 		}
 
 		// mark the tris on the best strip as used
-		for (j=0 ; j<bestlen ; j++)
+		for (j = 0; j < bestlen; j++)
 			used[besttris[j]] = 1;
 
 		if (besttype == 1)
-			commands[numcommands++] = (bestlen+2);
+			commands[numcommands++] = (bestlen + 2);
 		else
-			commands[numcommands++] = -(bestlen+2);
+			commands[numcommands++] = -(bestlen + 2);
 
-		for (j=0 ; j<bestlen+2 ; j++)
+		for (j = 0; j < bestlen + 2; j++)
 		{
 			// emit a vertex into the reorder buffer
 			k = bestverts[j];
@@ -285,10 +285,10 @@ GL_MakeAliasModelDisplayLists
 void GL_MakeAliasModelDisplayLists (model_t *m, aliashdr_t *hdr)
 {
 	int		i, j;
-	int			*cmds;
-	trivertx_t	*verts;
+	int *cmds;
+	trivertx_t *verts;
 	char	cache[MAX_QPATH], fullpath[MAX_OSPATH];
-	FILE	*f;
+	FILE *f;
 
 	aliasmodel = m;
 	paliashdr = hdr;
@@ -297,10 +297,10 @@ void GL_MakeAliasModelDisplayLists (model_t *m, aliashdr_t *hdr)
 	// look for a cached version
 	//
 	Q_strlcpy (cache, "glquake/", sizeof(cache));
-	COM_StripExtension (m->name+strlen("progs/"), cache+strlen("glquake/"));
+	COM_StripExtension (m->name + strlen("progs/"), cache + strlen("glquake/"));
 	Q_strlcat (cache, ".ms2", sizeof(cache));
 
-	COM_FOpenFile (cache, &f);	
+	COM_FOpenFile (cache, &f);
 	if (f)
 	{
 		fread (&numcommands, 4, 1, f);
@@ -314,7 +314,7 @@ void GL_MakeAliasModelDisplayLists (model_t *m, aliashdr_t *hdr)
 		//
 		// build it from scratch
 		//
-		Com_Printf ("meshing %s...\n",m->name);
+		Com_Printf ("meshing %s...\n", m->name);
 
 		BuildTris ();		// trifans or lists
 
@@ -323,7 +323,8 @@ void GL_MakeAliasModelDisplayLists (model_t *m, aliashdr_t *hdr)
 		//
 		Com_sprintf (fullpath, sizeof(fullpath), "%s/%s", com_gamedir, cache);
 		f = fopen (fullpath, "wb");
-		if (!f) {
+		if (!f)
+		{
 			char gldir[MAX_OSPATH];
 
 			Com_sprintf (gldir, sizeof(gldir), "%s/glquake", com_gamedir);
@@ -350,11 +351,11 @@ void GL_MakeAliasModelDisplayLists (model_t *m, aliashdr_t *hdr)
 	paliashdr->commands = (byte *)cmds - (byte *)paliashdr;
 	memcpy (cmds, commands, numcommands * 4);
 
-	verts = Hunk_Alloc (paliashdr->numposes * paliashdr->poseverts 
-		* sizeof(trivertx_t) );
+	verts = Hunk_Alloc (paliashdr->numposes * paliashdr->poseverts
+		* sizeof(trivertx_t));
 	paliashdr->posedata = (byte *)verts - (byte *)paliashdr;
-	for (i=0 ; i<paliashdr->numposes ; i++)
-		for (j=0 ; j<numorder ; j++)
+	for (i = 0; i < paliashdr->numposes; i++)
+		for (j = 0; j < numorder; j++)
 			*verts++ = poseverts[i][vertexorder[j]];
 }
 
