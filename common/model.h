b14c364501d01d9af587f64663afa3646a7207d3
maraakate <emoaddict15@gmail.com>
2026-09-13 23:05:26 -0400

    Fix bad prototype found by DamianReloaded.

--- common/model.h
+++ common/model.h
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef __MODEL__
 #define __MODEL__
 
+#include "mathlib.h"
 #include "modelgen.h"
 #include "spritegn.h"
 
@@ -426,7 +427,7 @@ model_t *Mod_ForName (char *name, qboolean crash);
 void	*Mod_Extradata (model_t *mod);	// handles caching
 void	Mod_TouchModel (char *name);
 
-mleaf_t *Mod_PointInLeaf (float *p, model_t *model);
+mleaf_t *Mod_PointInLeaf (vec3_t p, model_t *model);
 byte	*Mod_LeafPVS (mleaf_t *leaf, model_t *model);
 
 #endif	// __MODEL__
